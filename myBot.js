26c4635d0c00c9b389935e87371d517021c6ce87
priyankathakrur221206@gmail.com <snippy066.priyankathakur221206@gmail.com>
2021-09-12 21:15:43 +0530

    Update myBot.js

--- myBot.js
+++ myBot.js
@@ -1,9 +1,22 @@
+
+const { Console } = require("console");
 const disc=require("discord.js");
 const cli=new disc.Client();
 
 cli.on('ready',()=>{
 
     console.log("connect" + cli.user.tag);
+
+    cli.user.setActivity("With Codes....");
+    
+    cli.guilds.forEach((guild)=>{
+        Console.log(guild.name);
+
+        guild.channels.forEach((channel)=>{
+           Console.log(` - ${channel.id}`);
+        })
+    })
+       
 })
 
-cli.login("ODcwOTczMzcwNzgxNTQ0NDU4.YQUjKQ.8okaDwNKIm-MN0dZqiwLjIqEZgo");
+cli.login("ODcwOTczMzcwNzgxNTQ0NDU4.YQUjKQ.NkGw0a7hT4UpjS4so0e_ARQmnUs");
